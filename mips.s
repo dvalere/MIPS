26d0adea8fa3c6d48bf5810aa1924c3b7252ea58
dvalere <127788870+dvalere@users.noreply.github.com>
2024-02-12 21:51:45 -0500

    Update mips.s

--- mips.s
+++ mips.s
@@ -1,6 +1,8 @@
 #n = 36, so use base 36 I think
 #M = 26, so delta is 26th uppercase and fancy B is 26th lowercase, aka A-Z and a-z
 #10-35 are A-Z, 36+ are 9+(x-35) (From letter or number-> base 36)
+#Need to: Go over each character, check if it's a "/", if not, check if its within valid range
+
 .data
 prompt: .asciiz "Enter: "
 slash: .asciiz "/"
