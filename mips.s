3d092e0c9a907d0f24cf42f83857794e38fa325c
dvalere <127788870+dvalere@users.noreply.github.com>
2024-02-12 19:49:44 -0500

    data

--- mips.s
+++ mips.s
@@ -1,7 +1,10 @@
 #n = 36, so use base 36 I think
 #M = 26, so delta is 26th uppercase and fancy B is 26th lowercase, aka A-Z and a-z
 #10-35 are A-Z, 36+ are 9+(x-35) (From letter or number-> base 36)
-
+.data
+slash: .asciiz "/"
+space: .asciiz " "
+nosuch: .asciiz "-"
 
 
 
