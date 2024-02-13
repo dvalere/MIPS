45d4b1f404717bbd37cdaa39c57e5abac4e0a20d
dvalere <127788870+dvalere@users.noreply.github.com>
2024-02-12 21:48:39 -0500

    Update mips.s

--- mips.s
+++ mips.s
@@ -2,6 +2,7 @@
 #M = 26, so delta is 26th uppercase and fancy B is 26th lowercase, aka A-Z and a-z
 #10-35 are A-Z, 36+ are 9+(x-35) (From letter or number-> base 36)
 .data
+prompt: .asciiz "Enter: "
 slash: .asciiz "/"
 space: .asciiz " "
 nosuch: .asciiz "-"
